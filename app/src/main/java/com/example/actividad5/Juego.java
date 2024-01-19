be9fe0e51bd75e1976832d8eccc7f499f05ac9ed
Ricardo Montañana Gómez <rmontanana@gmail.com>
2024-01-19 13:45:46 +0100

    refactor: :zap: poner method

--- app/src/main/java/com/example/actividad5/Juego.java
+++ app/src/main/java/com/example/actividad5/Juego.java
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
-
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
@@ -19,16 +18,15 @@ import org.w3c.dom.Text;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Juego extends AppCompatActivity{
+public class Juego extends AppCompatActivity {
 
-
-    int[] tabla = new int[]{
-            0,0,0,
-            0,0,0,
-            0,0,0
+    int[] tabla = new int[] {
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0
     };
     TextView textWin;
-    Integer[] matriz ;
+    Integer[] matriz;
 
     TextView pruebita;
     ToggleButton[] botones;
@@ -49,86 +47,67 @@ public class Juego extends AppCompatActivity{
         textWin = (TextView) findViewById(R.id.textWin);
         textWin.setVisibility(View.INVISIBLE);
 
-        matriz = new Integer[]{
-                R.id.boton00, R.id.boton01, R.id.boton02, R.id.boton10, R.id.boton11, R.id.boton12, R.id.boton20, R.id.boton21, R.id.boton22
+        matriz = new Integer[] {
+                R.id.boton00, R.id.boton01, R.id.boton02, R.id.boton10, R.id.boton11, R.id.boton12, R.id.boton20,
+                R.id.boton21, R.id.boton22
         };
 
         pruebita = (TextView) findViewById(R.id.pruebita);
 
-        botones = new ToggleButton[]{findViewById(R.id.boton00), findViewById(R.id.boton01), findViewById(R.id.boton02),
-                                    findViewById(R.id.boton10), findViewById(R.id.boton11), findViewById(R.id.boton12),
-                                    findViewById(R.id.boton20), findViewById(R.id.boton21), findViewById(R.id.boton22)};
+        botones = new ToggleButton[] { findViewById(R.id.boton00), findViewById(R.id.boton01),
+                findViewById(R.id.boton02),
+                findViewById(R.id.boton10), findViewById(R.id.boton11), findViewById(R.id.boton12),
+                findViewById(R.id.boton20), findViewById(R.id.boton21), findViewById(R.id.boton22) };
 
     }
 
-    /*public void escribir(View vw){
-        boolean jugador = true;
-        int botonPulsado = 0;
-        boolean fin = true;
-        int casillas = 0;
-        String holia;
-        int[] botnpuls = new int[9];
-        Random ran = new Random();
-        int pos = ran.nextInt(tabla.length);
+    /*
+     * public void escribir(View vw){
+     * boolean jugador = true;
+     * int botonPulsado = 0;
+     * boolean fin = true;
+     * int casillas = 0;
+     * String holia;
+     * int[] botnpuls = new int[9];
+     * Random ran = new Random();
+     * int pos = ran.nextInt(tabla.length);
+     * 
+     * while(fin){
+     * 
+     * botonPulsado = Arrays.asList(matriz).indexOf(vw.getId());
+     * if (botonPulsado == 1){
+     * break;
+     * }
+     * vw.setBackgroundResource(R.drawable.star);
+     * tabla[botonPulsado] = 1;
+     * casillas++;
+     * if (casillas >= 5){
+     * fin = false;
+     * }
+     * 
+     * botnpuls = new int[botonPulsado];
+     * }
+     * 
+     * 
+     * 
+     * }
+     */
 
-        while(fin){
-
-                botonPulsado = Arrays.asList(matriz).indexOf(vw.getId());
-                if (botonPulsado == 1){
-                    break;
-                }
-                vw.setBackgroundResource(R.drawable.star);
-                tabla[botonPulsado] = 1;
-                casillas++;
-                if (casillas >= 5){
-                    fin = false;
-                }
-
-            botnpuls = new int[botonPulsado];
-            }
-
-
-
-        }*/
-
-    public void poner(View vw){
-
-
-
-            if (contadorParImpar % 2 == 0){
-                int botonPulsado = Arrays.asList(matriz).indexOf(vw.getId());
-                if (botones[botonPulsado].isChecked()){
-                    botones[botonPulsado].setEnabled(false);
-                    casillas++;
-                    contadorParImpar++;
-                }
-                vw.setBackgroundResource(R.drawable.star);
-                tabla[botonPulsado] = 1;
-            } else {
-                int botonPulsado = Arrays.asList(matriz).indexOf(vw.getId());
-                if (botones[botonPulsado].isChecked()){
-                    botones[botonPulsado].setEnabled(false);
-                    casillas++;
-                    contadorParImpar++;
-                }
-                vw.setBackgroundResource(R.drawable.moon);
-                tabla[botonPulsado] = 2;
-            }
-
+    public void poner(View vw) {
+        int botonPulsado = Arrays.asList(matriz).indexOf(vw.getId());
+        if (botones[botonPulsado].isChecked()) {
+            botones[botonPulsado].setEnabled(false);
+            vw.setBackgroundResource(casillas % 2 == 0 ? R.drawable.star : R.drawable.moon);
+            tabla[botonPulsado] = casillas % 2 + 1;
+            casillas++;
+        }
     }
 
-
-    //public int comprobador(int matriz){
-    //    for (int i = 0; i< matriz.length;i++){
-    //        return new int[]{matriz[i]};
+    // public int comprobador(int matriz){
+    // for (int i = 0; i< matriz.length;i++){
+    // return new int[]{matriz[i]};
     //
-    //    }
-    //}
+    // }
+    // }
 
-    }
-
-
-
-
-
-
+}
